9ee2b71c51c6a8f7e8442bcf251bc0b815a75bdf
BuildTools <unconfigured@null.spigotmc.org>
2020-08-14 19:10:18 -0700

    HashTable implementation

--- HashTable.py
+++ HashTable.py
@@ -1,3 +1,18 @@
-class HashTable:
-    #def __init__(self, size=512):
-        #self.arr = arr[size]
+class HashTable(object):
+    def __init__(self, size=128):
+        self.array = [None] * size
+
+    def hash(self, key):
+        length = len(self.array)
+        return hash(key) % length
+
+    def insert(self, key, value):
+        index = self.hash(key)
+        self.array[index] = [key, value]
+
+    def find(self, key):
+        index = self.hash(key)
+        if self.array[index] is None:
+            raise KeyError()
+        else:
+            return self.array[index][1]
