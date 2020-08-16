aa9cfda53288374ec72feff6477db95dd1006c13
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 21:06:14 -0700

    Added 2nd Hash Function and updated hash function per row

--- classes/CountMinSketch.py
+++ classes/CountMinSketch.py
@@ -1,4 +1,5 @@
 import numpy as np
+import hashlib
 
 class CountMinSketch(object):
     def __init__(self, hash, buckets):
@@ -11,10 +12,13 @@ class CountMinSketch(object):
             self.table[i, self.hash(key, i)] += 1
 
     def hash(self, key, i):
-        return (hash(key) + i) % self.buckets
+        return (hash(key) + self.hash2(key) * i) % self.buckets
 
     def count(self, key):
         min_count = self.table[0, self.hash(key, 0)]
         for i in range(1, self.hash):
             min_count = min(min_count, self.table[i, self.hash(key, i)])
         return min_count
+
+    def hash2(self, key):
+        return int(hashlib.md5(key).hexdigest()[:8], 16)
